ed7bf8fa9ae77cdadf5366bd992adcce8ce44b65
Solskin <cph-mm712@cphbusiness.dk>
2024-12-04 11:05:31 +0100

    fixes get order endpoint

--- Ordering.API/Controllers/OrderController.cs
+++ Ordering.API/Controllers/OrderController.cs
@@ -1,20 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
-using Ordering.API.Repositories;
+using Ordering.Domain.Interfaces;
+
 namespace Ordering.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
     public class OrderController : ControllerBase
     {
-        private readonly OrderRepository _orderRepository;
+        private readonly IOrderRepository _orderRepository;
 
-
-        public OrderController(OrderRepository orderRepository)
+        public OrderController(IOrderRepository orderRepository)
         {
             _orderRepository = orderRepository;
         }
 
-        [HttpGet("{id}")]
+        [HttpGet("{id:guid}")]
         public IActionResult GetOrder(Guid id)
         {
             var order = _orderRepository.GetByIdAsync(id, CancellationToken.None).Result;
